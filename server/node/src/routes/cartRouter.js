19a3513ab0de089d5718f135418c6638196c63ab
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-06-02 19:22:40 +0530

    Update cartRouter.js

--- server/node/src/routes/cartRouter.js
+++ server/node/src/routes/cartRouter.js
@@ -9,7 +9,7 @@ const cartRouter = express.Router()
 
 cartRouter.post('/add', async (req, res) => {
     try {
-        const old = await cart.findOne({ user_id: req.body.user_id, product_id: req.body.product_id, status: 0 })
+        const old = await cart.findOne({ user_id: req.body.user_id, product_id: req.body.product_id, status: 0, date: null })
         if (old) {
             return res.status(400).json({
                 success: false,
@@ -63,12 +63,12 @@ cartRouter.get('/view_cart/:id', async (req, res) => {
                 "$unwind": "$product"
             },
             {
-                "$match":{
-                    "user_id":new objectId(id)
+                "$match": {
+                    "user_id": new objectId(id)
                 }
             },
             {
-                "$match":{
+                "$match": {
                     "status": "0"
                 }
             },
@@ -81,24 +81,24 @@ cartRouter.get('/view_cart/:id', async (req, res) => {
                     'description': { '$first': '$product.description' },
                     'photo': { '$first': '$product.photo' },
                     'price': { '$first': '$product.price' },
-                   
+
                 }
             }
         ])
 
         data.forEach((item) => {
             item.total = item.price * item.quantity;
-          });
+        });
 
-          let totalValue = 0;
+        let totalValue = 0;
 
-          for (const item of data) {
+        for (const item of data) {
             totalValue += item.total;
-          }
+        }
 
-          data.forEach((item) => {
+        data.forEach((item) => {
             item.total_amount = totalValue;
-          });
+        });
 
         if (data[0] === undefined) {
             return res.status(401).json({
@@ -112,7 +112,7 @@ cartRouter.get('/view_cart/:id', async (req, res) => {
                 success: true,
                 error: false,
                 data: data,
-                
+
             })
         }
 
@@ -160,16 +160,16 @@ cartRouter.get('/decrement/:id', async (req, res) => {
     try {
         const id = req.params.id
         const old = await cart.findOne({ _id: id })
-        if(old.quantity>1){
+        if (old.quantity > 1) {
             const counts = old.quantity - 1
 
             const add = await cart.updateOne({ _id: id }, { $set: { quantity: counts } })
-    
+
             if (add.modifiedCount === 1) {
                 const old_product = await product.findOne({ _id: old.product_id })
                 const available_counts = old_product.quantity - 1
                 const products = await product.updateOne({ _id: old.product_id }, { $set: { quantity: available_counts } })
-    
+
                 return res.status(201).json({
                     success: true, error: false,
                     message: "updated"
@@ -186,7 +186,7 @@ cartRouter.get('/decrement/:id', async (req, res) => {
                 message: "Quantity cannot be less than 1"
             })
         }
-       
+
     }
     catch (err) {
         res.status(500).json({ success: false, error: true, message: 'Something Went Wrong' })
@@ -236,23 +236,34 @@ cartRouter.post('/proceed-to-buy/:id', async (req, res) => {
     }
 })
 
-cartRouter.post('/proceed-to-buy/:id', async (req, res) => {
+cartRouter.get('/buy_now/:id', async (req, res) => {
     try {
         const id = req.params.id
-        console.log(id);
-        const carts = await cart.find({ user_id: id, status: 0 })
-        for (let i = 0; i < carts.length; i++) {
-            const c_id = carts[i]._id
-            await cart.updateOne({ _id: c_id }, { $set: { status: 1 } })
+        const data = await cart.find({ user_id: id, status: 0 })
+        const dateString = new Date()
+        const date = new Date(dateString);
+        const formattedDate = date.toISOString().split('T')[0];
+        for (const item of data) {
+            await cart.updateOne(
+                { _id: item._id },
+                { $set: { status: 1, date: formattedDate } }
+            );
         }
-        res.status(201).json({
-            success: true, error: false,
-            message: 'Complete'
+
+
+        return res.status(401).json({
+            success: false,
+            error: true,
+            message: "Order Placed!"
         })
-    }
-    catch (err) {
-        res.status(500).json({ success: false, error: true, message: 'Something Went Wrong' })
-        console.log(err)
+
+
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error: true,
+            message: "Something went wrong"
+        })
     }
 })
 
