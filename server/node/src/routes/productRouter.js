45f8d8e6cf8040f4602b2e552d903b7e831c8a81
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-06-04 11:28:25 +0530

    Update productRouter.js

--- server/node/src/routes/productRouter.js
+++ server/node/src/routes/productRouter.js
@@ -1,101 +1,130 @@
 const express = require('express')
-const bcrypt=require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const product = require('../models/product')
 const productlist = require('../models/productlist')
 const cart = require('../models/cart')
-const productRouter=express.Router()
+const productRouter = express.Router()
 
 
-productRouter.get('/view-productdetails',(req,res) => {
+productRouter.get('/view-productdetails', (req, res) => {
     product.find()
-    .then(function(data) {
-        if(data==0) {
+        .then(function (data) {
+            if (data == 0) {
+                return res.status(401).json({
+                    success: false,
+                    error: true,
+                    message: "No data found"
+                })
+            }
+
+            else {
+                return res.status(200).json({
+                    success: true,
+                    error: false,
+                    data: data
+                })
+            }
+        })
+})
+productRouter.get('/view_singleproductdetails/:id', (req, res) => {
+    const id = req.params.id
+    product.find({ _id: id })
+        .then(function (data) {
+            if (data == 0) {
+                return res.status(401).json({
+                    success: false,
+                    error: true,
+                    message: "No Data Found!"
+                })
+            }
+            else {
+                return res.status(200).json({
+                    success: true,
+                    error: false,
+                    data: data
+                })
+            }
+        })
+})
+
+productRouter.get('/offer_product/', async(req, res) => {
+    
+  try {
+   const pro= await product.find()
+   const data  = pro.filter((data)=>{
+    return data.offerdetails!=null
+   })
+    .then(function (data) {
+        if (data[0] == undefined) {
             return res.status(401).json({
                 success: false,
                 error: true,
-                message:"No data found"
+                message: "No Data Found!"
             })
         }
-
-        else{
+        else {
             return res.status(200).json({
-                success:true,
-                error:false,
-                data:data
-            })
-        }
-    })
-})
-productRouter.get('/view_singleproductdetails/:id',(req,res)=>{
-    const id=req.params.id
-    product.find({_id:id})
-    .then(function (data) {
-        if(data == 0){
-            return res.status(401).json({
-                success:false,
-                error:true,
-                message:"No Data Found!"
-            })
-        }
-          else{
-            return res.status(200).json({
-                success:true,
-                error:false,
+                success: true,
+                error: false,
                 data: data
-          })
-                }
-                 })
+            })
+        }
+    })
+  } catch (error) {
+    
+  }
 })
-productRouter.post('/update_offer_details/:id',(req,res)=>{
-    const {offerdetails}= req.body
-    const id= req.params.id
+
+productRouter.post('/update_offer_details/:id', (req, res) => {
+    const { offerdetails } = req.body
+    const id = req.params.id
     console.log(id);
-    product.updateOne({_id: id},{ $set:{offerdetails} }).then((data)=>{
+    product.updateOne({ _id: id }, { $set: { offerdetails } }).then((data) => {
         console.log(data);
         res.status(200).json({
             success: true,
             error: false,
-            message:"Offer Updated"
+            message: "Offer Updated"
         })
-    }).catch(err=>{
+    }).catch(err => {
         return res.status(401).json({
-          message: "something went wrong"  
+            message: "something went wrong"
         })
     })
- })  
- 
- productRouter.get('/delete_offer_details/:id',(req,res)=>{
-    const id= req.params.id
+})
+
+productRouter.get('/delete_offer_details/:id', (req, res) => {
+    const id = req.params.id
     console.log(id);
-    product.updateOne({_id: id},{ $set:{offerdetails:null} }).then((data)=>{
+    product.updateOne({ _id: id }, { $set: { offerdetails: null } }).then((data) => {
         console.log(data);
         res.status(200).json({
             success: true,
             error: false,
-            message:"Details Updated"
+            message: "Details Updated"
         })
-    }).catch(err=>{
+    }).catch(err => {
         return res.status(401).json({
-          message: "something went wrong"  
+            message: "something went wrong"
         })
     })
- })
- productRouter.post('/add_to_cart/:id',(req,res)=>{
-    const {offerdetails}= req.body
-    const id= req.params.id
+})
+productRouter.post('/add_to_cart/:id', (req, res) => {
+    const { offerdetails } = req.body
+    const id = req.params.id
     console.log(id);
-    cart.updateOne({_id: id},{ $set:{offerdetails} }).then((data)=>{
+    cart.updateOne({ _id: id }, { $set: { offerdetails } }).then((data) => {
         console.log(data);
         res.status(200).json({
             success: true,
             error: false,
-            message:"Details Updated"
+            message: "Details Updated"
         })
-    }).catch(err=>{
+    }).catch(err => {
         return res.status(401).json({
-          message: "something went wrong"  
+            message: "something went wrong"
         })
     })
- })   
+})
 module.exports = productRouter
 
